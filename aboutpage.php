0b45fedb22b9e2e8b3efe9a84d6061c0121763a5
DanuVong <dvong@alphaworks.tech>
2022-06-15 18:11:43 -0700

    added profile and linkdin buttons

---
+++ aboutpage.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: About Page
+ *
+ * @package dv-portfolio-22
+ *
+ */
+?>
+<?php get_header(); ?> 
+<!-- Write HTML Here -->
---
+++ homepage.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: Homepage
+ *
+ * @package dv-portfolio-22
+ *
+ */
+?>
+<?php get_header(); ?> 
+<!-- Write HTML Here -->
+<main class="main-container">
+    <section class="about-page">
+    <div class="text-container">
+        <h3 class="logo">So So Dev</h3>
+        <h3 class="title">Junior Front-End Developer</h3>
+        
+
+        <div class="about-text">
+            <h1 class="hello">Hello, my name is Danu Vong</h1>
+            <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna fermentum iaculis eu non. Ac tincidunt vitae semper quis lectus nulla at.</p>
+            <button class="project-button" type="button">Projects</button>
+            <button class="linkdin-button" type="button">Linkdin</button>
+        </div>
+    </div>
+        <img class='personal-image' src='http://portfolio.local/wp-content/uploads/2022/06/bourdain.jpg' alt='personal photo'>
+    </section>
+    
+    <section class="projects-page"></section>
+    
+    <section class="contact-page"></section>
+
+</main>
