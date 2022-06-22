29c8622e7a684a8baabc8a94695f348a2c749def
DanuVong <dvong@alphaworks.tech>
2022-06-22 10:59:00 -0700

    pushed new project containers

--- homepage.php
+++ homepage.php
@@ -9,24 +9,44 @@
 <?php get_header(); ?> 
 <!-- Write HTML Here -->
 <main class="main-container">
-    <section class="about-page">
-    <div class="text-container">
-        <h3 class="logo">So So Dev</h3>
-        <h3 class="title">Junior Front-End Developer</h3>
-        
 
-        <div class="about-text">
-            <h1 class="hello">Hello, my name is Danu Vong</h1>
-            <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna fermentum iaculis eu non. Ac tincidunt vitae semper quis lectus nulla at.</p>
-            <button class="project-button" type="button">Projects</button>
-            <button class="linkdin-button" type="button">Linkdin</button>
+    <section class="about-page">
+        <div class="text-container">
+            <h3 class="logo">So So Dev</h3>
+            <h3 class="title">Junior Front-End Developer</h3>
+            
+            <div class="about-text">
+                <h1 class="hello">Hello, my name is Danu Vong</h1>
+                <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna fermentum iaculis eu non. Ac tincidunt vitae semper quis lectus nulla at.</p>
+                <button class="project-button" type="button">Projects</button>
+                <button class="linkdin-button" type="button">Linkedin</button>
+            </div>
         </div>
-    </div>
-        <img class='personal-image' src='http://portfolio.local/wp-content/uploads/2022/06/bourdain.jpg' alt='personal photo'>
+            <img class='personal-image' src='http://portfolio.local/wp-content/uploads/2022/06/Peace-e1655424389773.jpg'>
     </section>
     
-    <section class="projects-page"></section>
-    
+    <h1 class="projects-title">Projects</h1>
+
+    <section class="projects-page">
+     <div class="projects-container">
+        <h2>W4B Final</h2>
+        <p>fermentum iaculis eu non diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend</p>
+     </div>
+
+     <div class="projects-container">
+        <h2>Elk Grove Breweries</h2>
+     </div>
+
+     <div class="projects-container">
+        <h2>Space X Clone</h2>
+     </div>
+
+     <div class="projects-container">
+        <h2>Electried</h2>
+     </div>
+
+     </section>
+
     <section class="contact-page"></section>
 
 </main>
